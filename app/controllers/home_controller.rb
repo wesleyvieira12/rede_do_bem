8f40516e615d32669e138ea1b6814a6000eebfc2
Wesley Vieira <wesleyvieira12@gmail.com>
2017-02-27 22:05:02 -0300

    Mudando desing

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,10 @@
 class HomeController < ApplicationController
 	def index
-		@city = City.find(params[:id_cidade])
+		if session[:city_id].present?
+			@city = City.find(session[:city_id])
+		else
+			@city = City.find(params[:id_cidade])
+			session[:city_id] = @city.id
+		end
 	end
 end
