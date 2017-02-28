87d7fb01e865c9db3bb5a713d1e491deeb9b2fe8
Wesley Vieira <wesleyvieira12@gmail.com>
2017-02-28 10:59:38 -0300

    postgres

--- db/migrate/20170227024217_create_users.rb
+++ db/migrate/20170227024217_create_users.rb
@@ -2,7 +2,7 @@ class CreateUsers < ActiveRecord::Migration[5.0]
   def change
     create_table :users do |t|
       t.string :name
-      t.references :citiy, foreign_key: true
+      t.references :city, foreign_key: true
       t.references :category, foreign_key: true
       t.integer :kind
       t.integer :status
