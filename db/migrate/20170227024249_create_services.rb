8832ddbca3f5fa8bdd3358bc375e0f8813a993d3
Wesley Vieira <wesleyvieira12@gmail.com>
2017-02-28 11:42:09 -0300

    postgres

--- db/migrate/20170227024249_create_services.rb
+++ db/migrate/20170227024249_create_services.rb
@@ -2,8 +2,8 @@ class CreateServices < ActiveRecord::Migration[5.0]
   def change
     create_table :services do |t|
       t.string :description
-      t.references :user_client, foreign_key: true
-      t.references :user_professional, foreign_key: true
+      t.integer :user_client
+      t.integer :user_professional
       t.integer :status
 
       t.timestamps
