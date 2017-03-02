778d27d4b01715bec07f33afb802f2a4be52fa8a
Wesley Vieira <wesleyvieira12@gmail.com>
2017-03-02 10:44:56 -0300

    ALteração no desing do sistema

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,18 +5,18 @@ class HomeController < ApplicationController
 	def index
 		#Usuarios de uma determinada cidade
 		
-		@users = @q.result().where(kind:1, city_id: @city)
+		@users = @q.result().where(kind:1, status: "ativo", city_id: @city)
 		@users = @users.joins("INNER JOIN categories c ON users.category_id = c.id").distinct
 
 		
-		@comments = Comment.joins("INNER JOIN services s ON comments.service_id = s.id INNER JOIN users u ON u.id = s.user_professional_id AND u.city_id = #{@city.id}").order("id DESC").limit(5)
+		@comments = Comment.joins("INNER JOIN services s ON comments.service_id = s.id INNER JOIN users u ON u.id = s.user_professional_id AND u.city_id = #{@city.id} ").order("id DESC").limit(5)
 	end
 
 	def busca
 
 		if !params[:id].present?
 			
-	  		@users = @q.result().where(kind: 1, city_id: @city)
+	  		@users = @q.result().where(kind: 1, city_id: @city, status: "ativo")
   		else
   			
   			@users = User.where(category_id: params[:id])
@@ -25,11 +25,11 @@ class HomeController < ApplicationController
 
 	def show_professional
 		@user = User.find_by_id(params[:id])
-		@services = Service.where(user_professional_id: @user.id)
+		@services = Service.where(user_professional_id: @user.id, status: "ativo")
 	end
 	def show_service
 		@service = Service.find_by_id(params[:id])
-		@comments = Comment.where(service_id: @service.id)
+		@comments = Comment.where(service_id: @service.id, status: "ativo")
 	end
 	private
     
