77e0a86d246881d57f690ca344d16b9cccc0303d
Rodrigo <rodrigo19962010@live.com>
2017-03-02 12:05:52 -0300

    Add Status Permission for System

--- app/controllers/painel_controller.rb
+++ app/controllers/painel_controller.rb
@@ -1,8 +1,15 @@
 class PainelController < ApplicationController
+  
   before_action :authenticate_user!
+  before_action :active_user!
+
   def index
   	@count_user = User.count
   	@count_service = Service.count
   	@count_comment = Comment.count
+  end
+
+  def active_user!
+  	current_user.status=="ativo"
   end
 end
