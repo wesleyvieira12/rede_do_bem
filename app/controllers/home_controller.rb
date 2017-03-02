a217644e08f80ab45592e111a5efe5e58a305719
Wesley Vieira <wesleyvieira12@gmail.com>
2017-03-02 08:36:08 -0300

    Alteração na busca

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,7 +4,9 @@ class HomeController < ApplicationController
 		#Usuarios de uma determinada cidade
 		@q = User.ransack(params[:q])
 		@users = @q.result().where(kind:1, city_id: @city)
-		@users = @users.select(:category_id).distinct
+		@users = @users.joins("INNER JOIN categories c ON users.category_id = c.id").distinct
+
+		@categories = Category.joins("INNER JOIN users u ON u.category_id = categories.id AND u.city_id = #{@city.id}")
 
 		
 		@comments = Comment.joins("INNER JOIN services s ON comments.service_id = s.id INNER JOIN users u ON u.id = s.user_professional_id AND u.city_id = #{@city.id}").order("id DESC").limit(5)
@@ -33,8 +35,12 @@ class HomeController < ApplicationController
       		end
 
 		else
-			@city = City.find(params[:id_cidade])
-			session[:city_id] = @city.id
+			if params[:id_cidade].present?
+				@city = City.find(params[:id_cidade])
+				session[:city_id] = @city.id
+			else
+				redirect_to root_url
+			end
 		end
     end
 
