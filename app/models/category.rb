d887a72b125fdc297c68e98061d1c274686e256a
Rodrigo <rodrigo19962010@live.com>
2017-03-03 07:33:03 -0300

    Add Photo To Category

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,7 @@
 class Category < ApplicationRecord
+	require 'carrierwave/orm/activerecord'
+
+	mount_uploader :foto, FotoUploader
 	#Status : 0 (Ativo) e 1 (Inativo)
 	enum status: [:ativo, :inativo]
 	has_many :users
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170303101444) do
+ActiveRecord::Schema.define(version: 20170303102831) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
     t.integer  "status"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "foto"
   end
 
   create_table "cities", force: :cascade do |t|
