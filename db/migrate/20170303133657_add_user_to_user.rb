62741fdc58e407277da1dacbbe24c6b0b36af995
Wesley Vieira <wesleyvieira12@gmail.com>
2017-03-03 21:20:47 -0300

    postgres

--- db/migrate/20170303133657_add_user_to_user.rb
+++ db/migrate/20170303133657_add_user_to_user.rb
@@ -1,5 +1,5 @@
 class AddUserToUser < ActiveRecord::Migration[5.0]
   def change
-    add_reference :users, :professional, foreign_key: true
+    add_column :users, :professional_id, :integer
   end
 end
