91b7a2f21de945cfa8ef2046360b4697e5c29bd2
Rodrigo <rodrigo19962010@live.com>
2017-03-06 11:05:11 -0300

    Add Paginate to View

--- app/controllers/contatos_controller.rb
+++ app/controllers/contatos_controller.rb
@@ -6,6 +6,7 @@ class ContatosController < ApplicationController
   def index
     @q = Contato.ransack(params[:q])
     @contatos = @q.result(distinct: true)
+    @contatos = @contatos.paginate(:page => params[:page], :per_page => 10)
   end
 
   # GET /contatos/1
