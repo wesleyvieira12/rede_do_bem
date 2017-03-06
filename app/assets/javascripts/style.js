70cc0d4574acad1578f135ee088d95fa5becc937
Wesley Vieira <wesleyvieira12@gmail.com>
2017-03-06 09:07:44 -0300

    Retirando google maps

--- app/assets/javascripts/style.js
+++ app/assets/javascripts/style.js
@@ -3,12 +3,7 @@ jQuery(document).ready(function () {
 
     $('#carouselHacked').carousel();
 
-    //this code is for the gmap
-    var map = new GMaps({
-        el: '#map',
-        lat: -12.043333,
-        lng: -77.028333
-    });
+    
 
 
     //this code is for smooth scroll and nav selector
